29d3848131ca5ae4b1ea9c285cd4c29e69b0087f
Tech User <Tech User>
2022-08-07 16:10:10 -0700

    fix alias path issues

--- addon-scripts/initial-setup.sh
+++ addon-scripts/initial-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 PROJECTPATH=$HOME/projects/os-config
-ALIASFUNCTION="if [ -f ~/.alias ]; then . ~/.alias fi"
+ALIASFUNCTION="if [ -f ~/.alias ]; then . ~/.alias; fi"
 
 #------------------------------------------------------------------------------------
 #add supporting function file
@@ -47,13 +47,10 @@ then
 	strcolor g  "alias setup already available. existing.."
 else
 	strcolor y  "alias setup not found. setting up.."
-# Done	strcolor p "//TODO with replace test.txt with $HOME/.bashrc TODO//"
 cat >>  $HOME/.bashrc << EOL
-if [ -f ~/.alias ]; then . ~/.alias fi
+if [ -f ~/.alias ]; then . ~/.alias; fi
 EOL
-# Done	strcolor p  "// TODO replace test.txt with $PROJECTPATH/setup-files/.alias to $HOME/.alias TODO//"
 	cp $PROJECTPATH/setup-files/.alias $HOME
-# Done  strcolor p "//TODO source and replace test.txt with $HOME/.bashrc TODO//"
 	source $HOME/.bashrc
         strcolor g  "alias setup complete."
 fi
