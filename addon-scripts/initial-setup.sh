ee445a86499b727090ea9a6a28b89b75635e0c7c
Tech User <Tech User>
2022-08-07 12:20:48 -0700

    corrected path for alias and bashrc

--- addon-scripts/initial-setup.sh
+++ addon-scripts/initial-setup.sh
@@ -44,17 +44,17 @@ then
 	strcolor g  "alias setup already available. existing.."
 else
 	strcolor y  "alias setup not found. setting up.."
-	strcolor p "//TODO with replace test.txt with $HOME/.bashrc TODO//"
-cat >> "$PROJECTPATH"/setup-files/test.txt << EOL
+# Done	strcolor p "//TODO with replace test.txt with $HOME/.bashrc TODO//"
+cat >> "$HOME/.bashrc << EOL
 	# for custom alias setup
 	if [ -f ~/.alias ]; then
 	   . ~/.alias
 	fi
 EOL
-	strcolor p  "// TODO replace test.txt with $PROJECTPATH/setup-files/.alias to $HOME/.alias TODO//"
-	cp "$PROJECTPATH"/setup-files/test.txt $HOME
+# Done	strcolor p  "// TODO replace test.txt with $PROJECTPATH/setup-files/.alias to $HOME/.alias TODO//"
+	cp "$PROJECTPATH"/setup-files/.alias $HOME
         strcolor p "//TODO source and replace test.txt with $HOME/.bashrc TODO//"
-#	source "$PROJECTPATH"/setup-files/test.txt
+# Done	source $HOME/.bashrc
         strcolor g  "alias setup complete."
 fi
 
