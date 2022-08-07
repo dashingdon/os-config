2fcf6eae1e9394d9b8d3f6c1a78085be99e665d6
Tech User <Tech User>
2022-08-07 13:47:18 -0700

    adding export DEBIAN_FRONTEND=noninteractive and changed installation to quite mode in packagecheck function

--- addon-scripts/helper.sh
+++ addon-scripts/helper.sh
@@ -52,7 +52,7 @@ pkgcheck()
 	        	strcolor g ""$line" is installed "
 		else
 	        	strcolor y ""$line" not found. Installing ..."
-		        sudo apt install -q -y $line
+		        DEBIAN_FRONTEND=noninteractive apt install -q -y $line
 		fi
 	done
 
