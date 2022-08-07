fea728d82dfc7f75396a8c2d98b16ced22aa6c86
Tech User <Tech User>
2022-08-07 00:34:55 -0700

    alias setup draft. need path update

--- addon-scripts/initial-setup.sh
+++ addon-scripts/initial-setup.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 PROJECTPATH=$HOME/projects/os-config
+ALIASFUNCTION="
+	# for custom alias setup
+	if [ -f ~/.alias ]; then
+	   . ~/.alias
+	fi
+"
 
 #------------------------------------------------------------------------------------
 #add supporting function file
@@ -21,9 +27,6 @@ echo
 displaylist "$PROJECTPATH"/setup-files/packages.txt
 echo
 
-#if [[ "no" == $(ask_yes_or_no "Are you sure?") || \
-#      "no" == $(ask_yes_or_no "Are you *really* sure?") ]]
-
 if [[ "no" == $(ask_yes_or_no "Are you sure?") ]]
 then
     echo "Skipped."
@@ -31,6 +34,29 @@ then
 fi
 
 pkgcheck "$PROJECTPATH"/setup-files/packages.txt
+
+#------------------------------------------------------------------------------------
+# alias check and setup
+
+echo
+if grep -Fxq "$ALIASFUNCTION" "$PROJECTPATH"/setup-files/test.txt
+then
+	strcolor g  "alias setup already available. existing.."
+else
+	strcolor y  "alias setup not found. setting up.."
+	strcolor p "//TODO with replace test.txt with $HOME/.bashrc TODO//"
+cat >> "$PROJECTPATH"/setup-files/test.txt << EOL
+	# for custom alias setup
+	if [ -f ~/.alias ]; then
+	   . ~/.alias
+	fi
+EOL
+	strcolor p  "// TODO replace test.txt with $PROJECTPATH/setup-files/.alias to $HOME/.alias TODO//"
+	cp "$PROJECTPATH"/setup-files/test.txt $HOME
+        strcolor p "//TODO with replace test.txt with $HOME/.bashrc TODO//"
+#	source "$PROJECTPATH"/setup-files/test.txt
+        strcolor g  "alias setup complete."
+fi
 
 
 }
--- setup.sh
+++ setup.sh
@@ -33,47 +33,7 @@ case $n in
   *) echo "invalid option";;
 esac
 
-
-
-#echo
-#echo "following packages are necessary for minimal openbox installation. Packages are listed in " \
-#"the" $PROJECTPATH/setup-files/packages.txt \
-#"please review before proceeding further"
-
-#echo
-#displaylist "$PROJECTPATH"/setup-files/packages.txt
-#echo
-
-##if [[ "no" == $(ask_yes_or_no "Are you sure?") || \
-##      "no" == $(ask_yes_or_no "Are you *really* sure?") ]]
-
-#if [[ "no" == $(ask_yes_or_no "Are you sure?") ]]
-#then
-#    echo "Skipped."
-#    exit 0
-#fi
-
-#pkgcheck "$PROJECTPATH"/setup-files/packages.txt
-
 #------------------------------------------------------------------------------------
-
-
-: ' comment block of code
-echo "select the operation ************"
-echo "  1)operation 1"
-echo "  2)operation 2"
-echo "  3)operation 3"
-echo "  4)operation 4" 
-
-read n
-case $n in
-  1) echo "You chose Option 1";;
-  2) echo "You chose Option 2";;
-  3) echo "You chose Option 3";;
-  4) echo "You chose Option 4";;
-  *) echo "invalid option";;
-esac
-'
 
 # create required directories
 #echo "Creating directories"
@@ -82,11 +42,4 @@ esac
 
 
 #clone git repos
-
-: '
-strcolor g "Installing additional packages ..."
-echo
-pkgcheck ~/scripts/packs.txt
-echo
-'
 
