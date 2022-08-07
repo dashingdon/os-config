bd7f822c6b1511a2e838ce965efbe26537adabaa
Tech User <Tech User>
2022-08-07 14:43:18 -0700

    fix alias path issues

--- addon-scripts/initial-setup.sh
+++ addon-scripts/initial-setup.sh
@@ -19,7 +19,7 @@ colorset
 initial-setup()
 {
 
-export DEBIAN_FRONTEND=noninteractive
+#export DEBIAN_FRONTEND=noninteractive
 
 strcolor g "running system update"
 echo
@@ -32,7 +32,7 @@ echo "following packages are necessary for minimal openbox installation. Package
 "please review before proceeding further"
 
 echo
-displaylist "$PROJECTPATH"/setup-files/packages.txt
+displaylist $PROJECTPATH/setup-files/packages.txt
 echo
 
 if [[ "no" == $(ask_yes_or_no "Are you sure?") ]]
@@ -41,28 +41,28 @@ then
     exit 0
 fi
 
-pkgcheck "$PROJECTPATH"/setup-files/packages.txt
+pkgcheck $PROJECTPATH/setup-files/packages.txt
 
 #------------------------------------------------------------------------------------
 # alias check and setup
 
 echo
-if grep -Fxq "$ALIASFUNCTION" "$HOME"/.bashrc 
+if grep -Fxq $ALIASFUNCTION $HOME/.bashrc 
 then
 	strcolor g  "alias setup already available. existing.."
 else
 	strcolor y  "alias setup not found. setting up.."
 # Done	strcolor p "//TODO with replace test.txt with $HOME/.bashrc TODO//"
-cat >> "$HOME"/.bashrc << EOL
+cat >>  $HOME/.bashrc << EOL
 	# for custom alias setup
 	if [ -f ~/.alias ]; then
 	   . ~/.alias
 	fi
 EOL
 # Done	strcolor p  "// TODO replace test.txt with $PROJECTPATH/setup-files/.alias to $HOME/.alias TODO//"
-	cp "$PROJECTPATH"/setup-files/.alias "$HOME"
+	cp $PROJECTPATH/setup-files/.alias $HOME
 # Done  strcolor p "//TODO source and replace test.txt with $HOME/.bashrc TODO//"
-	source "$HOME/.bashrc"
+	source $HOME/.bashrc
         strcolor g  "alias setup complete."
 fi
 
